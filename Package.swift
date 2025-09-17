4ff0e487b8566191e2cf754a47682f64317389f1
Can Aksoy <can@canaksoy.com>
2025-09-17 10:41:40 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,33 +3,37 @@ import PackageDescription
 
 let package = Package(
     name: "IdentifySDK",
-    platforms: [.iOS(.v14)],
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
-        .library(name: "IdentifySDK", targets: ["IdentifySDK"]) // import IdentifySDK
+        .library(
+            name: "IdentifySDK",
+            targets: ["IdentifySDK"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git",
-                 .upToNextMajor(from: "1.1.1700")),
-        .package(url: "https://github.com/daltoniam/Starscream.git",
-                 exact: "3.1.1"),
-        .package(url: "https://github.com/stasel/WebRTC.git",
-                 .upToNextMajor(from: "94.0.0"))
+        // OpenSSL — up to next major from 1.1.1700
+        .package(
+            url: "https://github.com/krzyzanowskim/OpenSSL.git",
+            .upToNextMajor(from: "1.1.1700")
+        ),
+        // Starscream — exact 3.1.1
+        .package(
+            url: "https://github.com/daltoniam/Starscream.git",
+            exact: "3.1.1"
+        ),
+        // WebRTC — up to next major from 94.0.0
+        .package(
+            url: "https://github.com/stasel/WebRTC.git",
+            .upToNextMajor(from: "94.0.0")
+        )
     ],
     targets: [
         .binaryTarget(
-            name: "IdentifySDKBinary",
-            url: "https://github.com/<org>/<repo>/releases/download/2.3.8/IdentifySDK.xcframework.zip",
-            checksum: "2681e8daf97024bdf1065e2e17948489797411cbbf91463c24672841702a1067"
-        ),
-        .target(
             name: "IdentifySDK",
-            dependencies: [
-                "IdentifySDKBinary",
-                "OpenSSL",
-                .product(name: "Starscream", package: "Starscream"),
-                .product(name: "WebRTC", package: "WebRTC")
-            ],
-            path: "Sources"
+            url: "https://github.com/2sworks/id24.tr-ios-sdk-spm/releases/download/2.3.9/IdentifySDK.xcframework.zip",
+            checksum: "2681e8daf97024bdf1065e2e17948489797411cbbf91463c24672841702a1067"
         )
     ]
 )
