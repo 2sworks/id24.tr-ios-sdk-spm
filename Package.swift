9188ed39b076b4bdc40890bb31a6dc6fa4537960
ayhanhakan <48881910+ayhanhakan@users.noreply.github.com>
2026-04-29 13:21:59 +0300

    Release 2.5.5
    - Update Package.swift with correct 2.5.5 URL and checksum
    - Restore wrapper format with OpenSSL, Starscream, WebRTC dependencies
    - Remove embedded XCFramework (now distributed via GitHub Release URL)

--- Package.swift
+++ Package.swift
@@ -1,17 +1,35 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "IdentifySDK",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
-        .library(name: "IdentifySDK", targets: ["IdentifySDK"])
+        .library(name: "IdentifySDK", targets: ["IdentifySDKWrapper"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git",
+                 .upToNextMajor(from: "1.1.1700")),
+        .package(url: "https://github.com/daltoniam/Starscream.git",
+                 exact: "3.1.1"),
+        .package(url: "https://github.com/stasel/WebRTC.git",
+                 .upToNextMajor(from: "94.0.0"))
     ],
     targets: [
         .binaryTarget(
             name: "IdentifySDK",
-            url: "https://github.com/2sworks/id24.tr-ios-sdk-spm/releases/download/2.5.5/IdentifySDK.xcframework.zip",
-            checksum: "42d7a083b6828782f81c27e82df7c5b09767dda3febb227570a1a863663769d6"
+            url: "https://github.com/2sworks/id24.tr-ios-sdk-spm/releases/download/2.5.5/IdentifySDK-2.5.5.xcframework.zip",
+            checksum: "08eebf3d3aa79b4a692eb526cc923eb63f2a6d6df409c2edc4c7e01a3ae547c4"
+        ),
+        .target(
+            name: "IdentifySDKWrapper",
+            dependencies: [
+                "IdentifySDK",
+                "OpenSSL",
+                .product(name: "Starscream", package: "Starscream"),
+                .product(name: "WebRTC", package: "WebRTC")
+            ],
+            path: "Sources"
         )
     ]
 )
